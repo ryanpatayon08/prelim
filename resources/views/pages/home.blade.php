523f961370b8add1f76d8aaac3248313d1da7700
ken licame <thecrimsondrive@gmail.com>
2021-09-22 19:07:51 +0800

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prelim</title>
+    <link rel="stylesheet" href="{{('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Welcome</h1>
+    <h4>
+        <a href="{{url('/home')}}">Home</a>
+        <a href="{{url('/profile')}}">Profile</a>
+        <a href="{{url('/about')}}">About</a>
+    </h4>
+</body>
+</html>
